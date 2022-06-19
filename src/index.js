3b9084ff253606f24966b95da82aa8ae2cba5015
raj_saikia <raj.saikia@synechron.com>
2022-06-19 19:59:37 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+
+function PaymentButton({amount = 100, name = 'raj saikia', contact = '8860430262',
+  email = 'raj.scyrus@gmail.com', address = 'Tezpur, Assam, India'}) {
+  const options = {
+    key: "rzp_test_HJG5Rtuy8Xh2NB",
+    amount: amount, //  = INR 1
+    name: "Acme Company",
+    description: "Acme Company payment mode",
+    image: "https://cdn.razorpay.com/logos/7K3b6d18wHwKzL_medium.png",
+    handler: function(response) {
+      alert(response.razorpay_payment_id);
+    },
+    prefill: {
+      name: name,
+      contact: contact,
+      email: email
+    },
+    notes: {
+      address: address
+    },
+    theme: {
+      color: "#F37254",
+      hide_topbar: false
+    }
+  };
+
+  const openPayModal = options => {
+    var rzp1 = new window.Razorpay(options);
+    rzp1.open();
+  };
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://checkout.razorpay.com/v1/checkout.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
+  return (
+    <>
+      <button onClick={() => openPayModal(options)}>Pay</button>
+    </>
+  );
+}
+
+export default PaymentButton;
